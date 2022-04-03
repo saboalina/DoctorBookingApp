c3ffdaa2c95924dcb7eb5055feb6e03cb9195225
saboalina <sb.alina.m@gmail.com>
2022-04-03 18:11:01 +0300

    Initial Commit

---
+++ DoctorAppointmentBookingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoctorAppointmentBookingApp
+//
+//  Created by Alina Sabo Brandus on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
