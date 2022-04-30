a0d3a27a986704fb3d7c1090008cfd571d265646
saboalina <sb.alina.m@gmail.com>
2022-04-30 09:47:30 +0300

    medical details screen changed

--- DoctorAppointmentBookingApp/View Controllers/Patient/PatientMedicalCenterDetailsViewController.swift
+++ DoctorAppointmentBookingApp/View Controllers/Patient/PatientMedicalCenterDetailsViewController.swift
@@ -19,25 +19,62 @@ class PatientMedicalCenterDetailsViewController: UIViewController {
     @IBOutlet weak var friProgramLabel: UILabel!
     @IBOutlet weak var satProgramLabel: UILabel!
     @IBOutlet weak var sunProgramLabel: UILabel!
+    @IBOutlet weak var servicesTableView: UITableView!
     
     var medicalCenter: MedicalCenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let services = medicalCenter.services.components(separatedBy: ",")
+        print("]]]]]]] \(services)")
         setLabels()
     }
     
     func setLabels() {
         nameLabel.text = medicalCenter.name
         addressLabel.text = medicalCenter.address
-        monProgramLabel.text = medicalCenter.mon
-        tueProgramLabel.text = medicalCenter.tue
-        wedProgramLabel.text = medicalCenter.wed
-        thuProgramLabel.text = medicalCenter.thu
-        friProgramLabel.text = medicalCenter.fri
-        satProgramLabel.text = medicalCenter.sat
-        sunProgramLabel.text = medicalCenter.sun
+        if medicalCenter.mon == "" {
+            monProgramLabel.text = "Closed"
+        } else {
+            monProgramLabel.text = medicalCenter.mon
+        }
+        
+        if medicalCenter.tue == "" {
+            tueProgramLabel.text = "Closed"
+        } else {
+            tueProgramLabel.text = medicalCenter.tue
+        }
+        
+        if medicalCenter.wed == "" {
+            wedProgramLabel.text = "Closed"
+        } else {
+            wedProgramLabel.text = medicalCenter.wed
+        }
+        
+        if medicalCenter.thu == "" {
+            thuProgramLabel.text = "Closed"
+        } else {
+            thuProgramLabel.text = medicalCenter.thu
+        }
+        
+        if medicalCenter.fri == "" {
+            friProgramLabel.text = "Closed"
+        } else {
+            friProgramLabel.text = medicalCenter.fri
+        }
+        
+        if medicalCenter.sat == "" {
+            satProgramLabel.text = "Closed"
+        } else {
+            satProgramLabel.text = medicalCenter.sat
+        }
+        
+        if medicalCenter.sun == "" {
+            sunProgramLabel.text = "Closed"
+        } else {
+            sunProgramLabel.text = medicalCenter.sun
+        }
 
     }
 
