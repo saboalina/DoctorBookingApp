4a04dfa61c3bab9f151a9fdcda70654c7e6529b2
saboalina <sb.alina.m@gmail.com>
2022-05-07 10:16:24 +0300

    reminder for appointment

--- DoctorAppointmentBookingApp/View Controllers/Patient/Home/PatientHomeViewController.swift
+++ DoctorAppointmentBookingApp/View Controllers/Patient/Home/PatientHomeViewController.swift
@@ -1,6 +1,7 @@
 
 import UIKit
 import Firebase
+import UserNotifications
 
 class PatientHomeViewController: UIViewController {
     
@@ -12,6 +13,7 @@ class PatientHomeViewController: UIViewController {
     
     var doctorViewModel = DoctorViewModel()
     var medicalCenterViewModel = MedicalCenterViewModel()
+    var appointmentViewModel = AppointmentViewModel()
     var filterViewModel = FiltersViewModel.shared
     
     var patient: Patient!
@@ -48,6 +50,23 @@ class PatientHomeViewController: UIViewController {
         }
     }
     
+    private var allAppointments = [Appointment]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.appointments = self.allAppointments
+            }
+        }
+    }
+        
+    var appointments = [Appointment]() {
+        didSet {
+            DispatchQueue.main.async {
+                //self.appointmentsTableView.reloadData()
+                self.checkDates()
+            }
+        }
+    }
+    
     let categories = ["Dermathology", "Endocrinology", "Ultrasound", "Dermathology", "Psychology", "Plastic surgery", "Nutrition", "Cardiology", "Ophthalmology", "Neurology", "Paediatrics", "Psychiatry"]
     
     override func viewDidLoad() {
@@ -65,6 +84,10 @@ class PatientHomeViewController: UIViewController {
 
         medicalCenterViewModel.getAllMedicalCenters(collectionID: "medicalCenters") { medicalCenters in
                 self.medicalCenters = medicalCenters
+        }
+        
+        appointmentViewModel.getAllAppointmentsForAPatient(collectionID: "appointments", patientEmail: patient.email) { appointments in
+                self.appointments = appointments
         }
 
     }
@@ -87,6 +110,84 @@ class PatientHomeViewController: UIViewController {
     
     @IBAction func searchButtonTapped(_ sender: Any) {
         self.performSegue(withIdentifier: "SearchPage", sender: nil)
+    }
+    
+    func checkDates() {
+        for appointment in appointments {
+            print("-----> \(appointment.date)")
+            let stringDate = appointment.date
+            let dateInfo = stringDate.components(separatedBy: "-")
+            let appointmentYear = Int(dateInfo[0])
+            let appointmentMonth = Int(dateInfo[1])
+            let appointmentDay = Int(dateInfo[2])
+                        
+            let date = Date().addingTimeInterval(86400)
+            let calendar = Calendar.current
+
+            let currentDateYear = calendar.component(.year, from: date)
+            let currentDateMonth = calendar.component(.month, from: date)
+            let currentDateDay = calendar.component(.day, from: date)
+
+
+            if currentDateYear == appointmentYear && currentDateMonth == appointmentMonth && currentDateDay == appointmentDay {
+                print("programare maine")
+                getNotification(appointment: appointment)
+            }
+        }
+    }
+    
+    
+    func getNotification(appointment: Appointment) {
+        
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Appointment tomorrow"
+        content.body = "You have an appointment for tomorrow at \(appointment.time). Check your appointments list!"
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "yourIdentifier"
+        content.userInfo = ["example": "information"] // You can retrieve this when displaying notification
+        
+        let date = convertDate(date: Date() + 5)
+                
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date as DateComponents, repeats: false)
+        
+        // Create request
+        let uniqueID = UUID().uuidString // Keep a record of this if necessary
+        let request = UNNotificationRequest(identifier: uniqueID, content: content, trigger: trigger)
+        center.add(request) // Add the notification request
+    }
+    
+    func convertDate(date: Date) -> NSDateComponents {
+        print(date)
+
+        let calendar = Calendar.current
+
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let sec = calendar.component(.second, from: date)
+
+        let newDate = NSDateComponents()
+
+        print("\(year):\(month):\(day)" + " " + "\(hour):\(minutes):\(sec)")
+
+        newDate.timeZone = TimeZone.current
+
+        newDate.hour = hour
+        newDate.minute = minutes
+        newDate.second = sec
+
+        newDate.day = day
+        newDate.month = month
+        newDate.year = year
+
+        print("\(newDate.year):\(newDate.month):\(newDate.day)" + " " + "\(newDate.hour):\(newDate.minute):\(newDate.second) zone:\(TimeZone.current)")
+
+        return newDate
     }
 }
 
