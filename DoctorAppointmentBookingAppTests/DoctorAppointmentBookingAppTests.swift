66aa41f8e3fee8fab82c7df45dff923ba1f02b88
saboalina <sb.alina.m@gmail.com>
2022-06-07 15:53:07 +0300

    medical centers unit test

--- DoctorAppointmentBookingAppTests/DoctorAppointmentBookingAppTests.swift
+++ DoctorAppointmentBookingAppTests/DoctorAppointmentBookingAppTests.swift
@@ -1,9 +1,3 @@
-//
-//  DoctorAppointmentBookingAppTests.swift
-//  DoctorAppointmentBookingAppTests
-//
-//  Created by Alina Sabo Brandus on 07.06.2022.
-//
 
 import XCTest
 
@@ -35,4 +29,31 @@ class DoctorAppointmentBookingAppTests: XCTestCase {
         
     }
 
+    func testgetAvailableMedicalCenters() {
+        
+        let startDate = Date(timeIntervalSinceReferenceDate: 676278924.117903)
+        let endDate = Date(timeIntervalSinceReferenceDate: 676454760.0)
+        
+        let medicalCenter1 = MedicalCenter(name: "", address: "", services: "", sun: "12:00-15:00", mon: "12:00-15:00", tue: "12:00-15:00", wed: "", thu: "", fri: "", sat: "", latitude: "", longitude: "", id: "", imageURL: "")
+        let medicalCenter2 = MedicalCenter(name: "", address: "", services: "", sun: "", mon: "", tue: "12:00-15:00", wed: "12:00-15:00", thu: "12:00-15:00", fri: "12:00-15:00", sat: "12:00-15:00", latitude: "", longitude: "", id: "", imageURL: "")
+        let medicalCenter3 = MedicalCenter(name: "", address: "", services: "", sun: "12:00-15:00", mon: "12:00-15:00", tue: "", wed: "12:00-15:00", thu: "", fri: "12:00-15:00", sat: "", latitude: "", longitude: "", id: "", imageURL: "")
+        let medicalCenter4 = MedicalCenter(name: "", address: "", services: "", sun: "", mon: "", tue: "12:00-15:00", wed: "", thu: "12:00-15:00", fri: "", sat: "12:00-15:00", latitude: "", longitude: "", id: "", imageURL: "")
+        let medicalCenter5 = MedicalCenter(name: "", address: "", services: "", sun: "", mon: "", tue: "", wed: "", thu: "", fri: "", sat: "", latitude: "", longitude: "", id: "", imageURL: "")
+        
+
+        var medicalCenters = [MedicalCenter]()
+        medicalCenters.append(medicalCenter1)
+        medicalCenters.append(medicalCenter2)
+        medicalCenters.append(medicalCenter3)
+        medicalCenters.append(medicalCenter4)
+        medicalCenters.append(medicalCenter5)
+        
+        let filters = FiltersViewModel.shared
+        filters.medicalCenters = medicalCenters
+        
+        let actual = filters.getAvailableMedicalCenters(startDate: startDate, endDate: endDate)
+        
+        XCTAssertEqual(actual.count, 1)
+        
+    }
 }
